a9bf1982689c5c3a4889acf814db8f9518900805
Paulo Moreira <phmoreira37@gmail.com>
2024-10-03 11:06:48 -0300

    put /livros/:id

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -27,6 +27,15 @@ class LivroController {
       res.status(500).json({ message: `${error.message} - falha ao cadastrar livro` })
     }
   }
+
+  static async atualizarLivro(req, res) {
+    try {
+      const livroAtualizado = await Livro.findByIdAndUpdate(req.params.id, req.body)
+      res.status(200).json({ message: "livro atualizado", livro: livroAtualizado })
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - falha na atualização` })
+    }
+  }
 }
 
 export default LivroController
