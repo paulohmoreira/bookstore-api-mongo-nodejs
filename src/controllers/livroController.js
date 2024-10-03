f4967f0130781a757357e28bf9f08b065a04c208
Paulo Moreira <phmoreira37@gmail.com>
2024-10-03 11:02:30 -0300

    post /livros

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -18,6 +18,15 @@ class LivroController {
       res.status(500).json({ message: `${error.message} - falha na requisição` })
     }
   }
+
+  static async cadastrarLivro(req, res) {
+    try {
+      const novoLivro = await Livro.create(req.body)
+      res.status(201).json({ message: "criado com sucesso", livro: novoLivro })
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - falha ao cadastrar livro` })
+    }
+  }
 }
 
 export default LivroController
