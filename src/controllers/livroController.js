a7422aff1bc15820ba8e7074e4c1e53c411fbae0
Paulo Moreira <phmoreira37@gmail.com>
2024-10-08 13:31:09 -0300

    filtro de busca por numeroPaginas em /livros

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -79,11 +79,7 @@ class LivroController {
 
   static listarLivroPorFiltro = async (req, res, next) => {
     try {
-      const { editora, titulo } = req.query;
-
-      const busca = {};
-      if (editora) busca.editora = editora;
-      if (titulo) busca.titulo = { $regex: titulo, $options: "i" };
+      const busca = processaBusca(req.query);
 
       const livrosResultado = await Livro.find(busca);
 
@@ -92,6 +88,20 @@ class LivroController {
       next(erro);
     }
   };
+}
+
+function processaBusca(parametros) {
+  const { editora, titulo, minPaginas, maxPaginas } = parametros;
+
+  const busca = {};
+  if (editora) busca.editora = editora;
+  if (titulo) busca.titulo = { $regex: titulo, $options: "i" };
+
+  if (minPaginas || maxPaginas) busca.numeroPaginas = {};
+  if (minPaginas) busca.numeroPaginas.$gte = minPaginas;
+  if (maxPaginas) busca.numeroPaginas.$lte = maxPaginas;
+
+  return busca;
 }
 
 export default LivroController;
